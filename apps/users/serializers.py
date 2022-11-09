ef92850c48720439df4af4d50c4aca374e9dd1d1
Nazar <odddellarobea@gmail.com>
2022-11-09 21:53:51 +0700

    add check username

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -78,6 +78,11 @@ class ClientSerializer(serializers.ModelSerializer):
         username = (
             f"{user_fields['first_name']}_{user_fields['last_name']}"
         )
+        if models.User.objects.filter(username=username).exists():
+            username = (
+                f"{user_fields['first_name']}_{user_fields['last_name']}"
+                f"_{validated_data['phone_number']}"
+            )
         user = models.User.objects.create(
             username=username,
             first_name=user_fields['first_name'],
