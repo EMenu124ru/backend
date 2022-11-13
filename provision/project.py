fd80467a3bc51b5cc2c0a2d1fb5f6d03a113c1c2
Nazar <odddellarobea@gmail.com>
2022-11-13 22:41:47 +0700

    Remove command for init

--- provision/project.py
+++ provision/project.py
@@ -1,26 +1,9 @@
 from invoke import task
 
-from . import common, django, docker
+from . import django
 
 
 @task
 def fill_sample_data(context, compose="dev"):
     """Prepare sample data for local usage."""
     django.manage(context, command="runscript fill_sample_data", compose=compose)
-
-
-@task
-def init(context, compose=docker.DEVELOPMENT_CONTAINER):
-    """Prepare env for working with project."""
-    docker.build(context, compose=compose)
-    django.manage(context, command="migrate", compose=compose)
-    if compose == docker.DEVELOPMENT_CONTAINER:
-        django.createsuperuser(context, compose=compose)
-    try:
-        fill_sample_data(context, compose=compose)
-    except NotImplementedError:
-        common.warn(
-            "Awesome, almost everything is Done! \n"
-            "You're the first developer - pls generate factories \n"
-            "for test data and setup development environment",
-        )
