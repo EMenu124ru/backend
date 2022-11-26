ca94cb1a4e02619ab87d180f6b91e87b52be857a
Danil Kokh <danil0420022@gmail.com>
2022-11-26 14:30:13 +0000

    Nice deploy, bro

--- .space.kts
+++ .space.kts
@@ -1,18 +1,18 @@
-job("Run npm test and publish") {
-    container(displayName = "Run publish script", image = "node:14-alpine") {
-        env["REGISTRY"] = "https://npm.pkg.jetbrains.space/mycompany/p/projectkey/mynpm"
+job("Run tests, build, publish") {
+    container(image = "python_custom_img:0.0.1") {
+        // specify URL of the package index using env var
+        env["URL"] = "https://pypi.pkg.jetbrains.space/ikit-ki20-161-b/p/team-course-project-2022-2023/python/simple"
+
+        // We suppose that your project has default build configuration -
+        // the built package is saved to the ./dist directory
         shellScript {
-            interpreter = "/bin/sh"
             content = """
-                echo Install npm dependencies...
-                npm ci
-                echo Run build if it exists in package.json...
-                npm run build --if-present
                 echo Run tests...
-                npm run test
-                echo Run publishing...
-                chmod +x ./publish.sh
-                ./publish.sh
+                pytest ./tests/
+                echo Build package...
+                python -m build
+                echo Upload package...
+                twine upload --repository-url ${'$'}URL -u ${'$'}JB_SPACE_CLIENT_ID -p ${'$'}JB_SPACE_CLIENT_SECRET dist/*
             """
         }
     }
