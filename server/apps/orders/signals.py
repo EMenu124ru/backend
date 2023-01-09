10648e1d8d890be098b2430c0ed20699f1e2a4c6
Alexey <alexeydzham@gmail.com>
2023-01-09 16:34:20 +0700

    Improved logic for changing statuses, should work properly now

--- server/apps/orders/signals.py
+++ server/apps/orders/signals.py
@@ -5,16 +5,11 @@ from apps.orders.models import OrderAndDishes, Order
 
 @receiver(post_save, sender=OrderAndDishes)
 def change_order_status_based_on_dishes(instance, created, **kwargs) -> None:
-    if created:
-        print("created")
-        return
-    print("is it working??")
     dishes_from_same_order = OrderAndDishes.objects.filter(
         order=instance.order,
     )
     for dish in dishes_from_same_order:
         if dish.status == OrderAndDishes.Statuses.COOKING:
-            print("changing order status to cooking")
             instance.order.status = Order.Statuses.COOKING
             return
     is_all_ready = True
@@ -23,5 +18,4 @@ def change_order_status_based_on_dishes(instance, created, **kwargs) -> None:
             is_all_ready = False
             break
     if is_all_ready:
-        print("changing order status to waiting for delivery")
         instance.order.status = Order.Statuses.WAITING_FOR_DELIVERY
