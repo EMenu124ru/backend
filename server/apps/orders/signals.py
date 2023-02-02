1c8fadf8e58a1378bbe4934978343046569bbea1
Alexey <alexeydzham@gmail.com>
2023-02-02 19:17:18 +0700

    replaced for loops with 'exists()'

--- server/apps/orders/signals.py
+++ server/apps/orders/signals.py
@@ -6,16 +6,12 @@ from apps.orders.models import OrderAndDishes, Order
 @receiver(post_save, sender=OrderAndDishes)
 def change_order_status_based_on_dishes(instance, created, **kwargs) -> None:
     dishes_from_same_order = OrderAndDishes.objects.filter(
-        order=instance.order,
+        order_id=instance.order.id,
     )
-    for dish in dishes_from_same_order:
-        if dish.status == OrderAndDishes.Statuses.COOKING:
-            instance.order.status = Order.Statuses.COOKING
-            return
+    if dishes_from_same_order.filter(status=OrderAndDishes.Statuses.COOKING).exists():
+        instance.order.status = Order.Statuses.COOKING
     is_all_ready = True
-    for dish in dishes_from_same_order:
-        if dish.status != OrderAndDishes.Statuses.DONE:
-            is_all_ready = False
-            break
+    if  not dishes_from_same_order.filter(status=OrderAndDishes.Statuses.DONE).exists():
+        is_all_ready = False
     if is_all_ready:
         instance.order.status = Order.Statuses.WAITING_FOR_DELIVERY
