809b5336ae0b0a4db161b43a0aeebce3e93d543a
Nazar <furnoxl@yandex.ru>
2023-03-01 20:27:30 +0700

    Replace func for check role of employee

--- server/apps/users/permissions.py
+++ server/apps/users/permissions.py
@@ -1,5 +1,15 @@
 from rest_framework import permissions
 
+from apps.users.models import User
+
+
+def check_role_employee(user: User, role: str) -> bool:
+    if user.is_client:
+        return False
+    if user.employee.role == role:
+        return True
+    return False
+
 
 class IsCurrentUser(permissions.BasePermission):
 
