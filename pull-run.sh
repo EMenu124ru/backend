50f3a85f1910979e4b85b3cf3a8263153de1cb35
Nazar <furnoxl@yandex.ru>
2023-04-12 21:24:07 +0700

    Change command for deploy

--- pull-run.sh
+++ pull-run.sh
@@ -1,3 +1,4 @@
 docker-compose -f ./docker-compose.prod.yml down --remove-orphans
 docker-compose -f ./docker-compose.prod.yml pull
 docker-compose -f ./docker-compose.prod.yml up -d --build
+docker-compose -f ./docker-compose.prod.yml run python manage.py migrate
