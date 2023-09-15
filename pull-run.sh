6e025c037b15d8bd34a9221733b4215bb2f5458b
Nazar <furnoxl@yandex.ru>
2023-09-15 11:56:07 +0700

    Add deploy in project

--- pull-run.sh
+++ pull-run.sh
@@ -1,4 +1,5 @@
 docker-compose -f ./docker-compose.prod.yml down --remove-orphans
+docker system prune -f
 docker-compose -f ./docker-compose.prod.yml pull
 docker-compose -f ./docker-compose.prod.yml up -d --build
 docker-compose -f ./docker-compose.prod.yml run django python manage.py migrate
