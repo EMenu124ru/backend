2f927e0bd7861e2cd4620bcad52b6c8bb0511a51
Nazar <furnoxl@yandex.ru>
2023-12-26 17:02:49 +0700

    Caching dishes

--- server/apps/orders/functions/__init__.py
+++ server/apps/orders/functions/__init__.py
@@ -1,5 +1,6 @@
-from .filter_dishes import get_available_dishes
+from .filter_dishes import get_available_dishes, get_or_create_cache_dishes
 
 __all__ = (
     get_available_dishes,
+    get_or_create_cache_dishes,
 )
