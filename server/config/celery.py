12566e54512f801781a89d0b57966a7eee7a21cd
Nazar <furnoxl@yandex.ru>
2024-05-20 08:14:30 +0700

    Fix commands for containers

--- server/config/celery.py
+++ server/config/celery.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 import os
-from datetime import timedelta
 
 from celery import Celery
 
@@ -13,10 +12,10 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'check_delayed_orders': {
         'task': 'apps.orders.tasks.delayed_orders.check_delayed_orders',
-        'schedule': timedelta(seconds=30),
+        'schedule': 30.0,
     },
     'send_updated_orders': {
         'task': 'apps.orders.tasks.update_orders.send_updated_orders',
-        'schedule': timedelta(seconds=10),
+        'schedule': 10.0,
     },
 }
--- server/config/development/__init__.py
+++ server/config/development/__init__.py
@@ -1,4 +1,5 @@
 from .celery import *  # noqa F403
 from .drf import *  # noqa F403
+from .fcm import *  # noqa F403
 from .installed_apps import *  # noqa F403
 from .settings import *  # noqa F403
